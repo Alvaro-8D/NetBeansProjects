a9097bb3c5f65c7aae2fe86abaf89d85a3409a7d
Álvaro Claudio <alvaro.claudio@educamadrid.org>
2025-05-03 19:23:26 +0200

    Empiezo EjClase propuesto en Teoria-Ficheros

--- Carpeta Unidad7/U7 Teoria/src/Hoja1/EnClase.java
+++ Carpeta Unidad7/U7 Teoria/src/Hoja1/EnClase.java
@@ -1,11 +1,46 @@
 package Hoja1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 
 public class EnClase 
 {
-
-/* ---------------------------- Constructores --------------------------------*/    
-
+    public static void main(String[] args) 
+    {
+        // "C:\ejemplos\javi.txt"
+        try
+        {
+            String frase;
+            
+            FileReader lectur = new FileReader("C:\\ejemplos\\javi.txt");
+        
+            BufferedReader entrada = new BufferedReader(lectur);
+            
+            frase = entrada.readLine();
+            System.out.println(frase);
+            
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch(RuntimeException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        
+        
+        
+        
+    }// main
 /* ------------------------------- Metodos ---------------------------------- */
 
 
