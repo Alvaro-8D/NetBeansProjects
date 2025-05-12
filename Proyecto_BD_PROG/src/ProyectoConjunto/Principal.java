6612b45c7607c16fba399717e4de87f2a20715d2
Álvaro Claudio <alvaro.claudio@educamadrid.org>
2025-05-13 00:11:28 +0200

    Añado practica de PROG y BD

---
+++ Proyecto_BD_PROG/src/ProyectoConjunto/Principal.java
@@ -0,0 +1,32 @@
+package ProyectoConjunto;
+
+import java.util.Scanner;
+
+
+public class Principal {
+    public static void main(String[] args) 
+    {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Elije una de las siguientes acciones en la Base de Datos:"
+                
+                + "\n\n(1) Nombres y sueldos de empleados con MAS de 3 Hijos\n"
+                
+                + "\n(2) Ver media de annos en la empresa de\n     "
+                + "un Departamento introducido por teclado\n"
+                
+                + "\n(3) Empleados que superan en sueldo\n     "
+                + "a Claudia Fierro en mas del 50%\n"
+                
+                + "\n(4) 5'33% de aumento del sueldo a empleados\n     "
+                + "y 6'19% de aumento de comision a vendedores\n"
+                
+                + "\n(5) Mostrar empleados de tabla TEMPLE que trabajan\n      "
+                + "en un Departamento introducido por teclado\n");
+        
+        
+        
+    }// main
+/* ------------------------------- Metodos ---------------------------------- */
+
+}// class
